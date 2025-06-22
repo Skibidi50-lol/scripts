fb89419955f800a73fc7d82e3d39103d5362baf3
Skibidi50-lol <realhatroi@gmail.com>
2025-06-22 12:02:40 +0700

    Create Noclip.lua

---
+++ Noclip.lua
@@ -0,0 +1,17 @@
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local noclipEnabled = true
+
+RunService.Stepped:Connect(function()
+    if noclipEnabled then
+        local player = Players.LocalPlayer
+        local character = player and player.Character
+        if character then
+            for _, part in pairs(character:GetDescendants()) do
+                if part:IsA("BasePart") and part.CanCollide then
+                    part.CanCollide = false
+                end
+            end
+        end
+    end
+end)
